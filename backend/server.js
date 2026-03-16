2817492c4f3280d49bd0895d64e526895ef1a56e
mo7324422-wq <mo7324422@gmail.com>
2026-03-17 00:37:52 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const db = require('./database');
 
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors({ origin: '*', credentials: true }));
